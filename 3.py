764927400a437fd0254d511cd84e0e60342e2ada
karanpanchal1710 <114278125+karanpanchal1710@users.noreply.github.com>
2022-10-15 11:42:21 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,11 @@
+from multiprocessing import Value
+
+
+try:
+    x=int(input('Enter a number upto 100'))
+    if x > 100:
+        raise ValueError(x)
+except ValueError:
+    print(x,"is out of allowed range")
+else:
+    print(x,("is within the allowed range"))    
